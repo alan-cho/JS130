bcdd413c12ef73d5b94b04bdb9ca72ee3ed68970
Alan Cho <alancho397@gmail.com>
2023-07-18 20:10:12 -0500

    reimplemented filter

--- filter.js
+++ filter.js
@@ -1,1 +1,10 @@
 //Reimplement Array.prototype.filter
+function filter(array, callback) {
+  let resultArray = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (callback(array[index])) {
+      resultArray.push(array[index]);
+    }
+  }
+  return resultArray;
+}
