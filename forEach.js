525f49cadfbe88cf62fb57ae31f50d4419c69e76
Alan Cho <alancho397@gmail.com>
2023-07-18 20:05:10 -0500

    implement function

--- forEach.js
+++ forEach.js
@@ -1,1 +1,6 @@
 // Reimplement the Array.prototype.forEach method.
+function forEach(array, callback, thisArg) {
+  for (let index = 0; index < array.length; index += 1) {
+    callback.call(thisArg, array[index], index, array);
+  }
+}
