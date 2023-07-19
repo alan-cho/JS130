6294b756ab86b1afacf0cce01aeb3fb576c18e7f
Alan Cho <alancho397@gmail.com>
2023-07-18 23:58:12 -0500

    added todolist class

--- todo.js
+++ todo.js
@@ -29,3 +29,72 @@ class Todo {
     return this.title;
   }
 }
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  add(toDoItem) {
+    if (toDoItem instanceof Todo) {
+      this.todos.push(toDoItem);
+    } else {
+      throw new TypeError("Can only add Todo objects.");
+    }
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.size() - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  _validateIndex(index) {
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`Invalid index: ${index}`);
+    }
+  }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every((todo) => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1)[0];
+  }
+
+  toString() {
+    let title = `---- ${this.title} ----`;
+    let list = this.todos.map((todo) => todo.toString()).join("\n");
+    return `${title}\n${list}`;
+  }
+}
