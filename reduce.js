b3f01794bcfcac1c672499933035c83cc90ecb5f
Alan Cho <alancho397@gmail.com>
2023-07-18 22:22:12 -0500

    initialize reduce.js, reimplement reduce

---
+++ reduce.js
@@ -0,0 +1,17 @@
+//Reimplement Array.prototype.reduce
+function reduce(array, callback, initialValue) {
+  let accumulator = initialValue;
+  let index = 0;
+
+  if (accumulator === undefined) {
+    accumulator = array[0];
+    index = 1;
+  }
+
+  while (index < array.length) {
+    accumulator = callback(accumulator, array[index]);
+    index += 1;
+  }
+
+  return accumulator;
+}
