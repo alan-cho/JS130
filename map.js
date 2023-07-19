e19c35b508a976389c7cb40077f462141650c46d
Alan Cho <alancho397@gmail.com>
2023-07-18 20:20:31 -0500

    reimplement map

--- map.js
+++ map.js
@@ -1,1 +1,8 @@
 //Reimplement Array.prototype.map
+function map(array, callback) {
+  let resultArray = [];
+  for (let index = 0; index < array.length; index += 1) {
+    resultArray.push(callback(array[index]));
+  }
+  return resultArray;
+}
