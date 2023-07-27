9932d4c9732c0c617bae2b35e31dd1596ae52f33
Alan Cho <alancho397@gmail.com>
2023-07-27 18:18:26 -0500

    refactor todo into two separate files; todo.js and todolist.js

--- todo.js
+++ todo.js
@@ -1,8 +1,4 @@
-//To-Do List
 class Todo {
-  static DONE_MARKER = "X";
-  static UNDONE_MARKER = " ";
-
   constructor(title) {
     this.title = title;
     this.done = false;
@@ -30,117 +26,7 @@ class Todo {
   }
 }
 
-class TodoList {
-  constructor(title) {
-    this.title = title;
-    this.todos = [];
-  }
+Todo.DONE_MARKER = "X";
+Todo.UNDONE_MARKER = " ";
 
-  add(toDoItem) {
-    if (toDoItem instanceof Todo) {
-      this.todos.push(toDoItem);
-    } else {
-      throw new TypeError("Can only add Todo objects.");
-    }
-  }
-
-  size() {
-    return this.todos.length;
-  }
-
-  first() {
-    return this.todos[0];
-  }
-
-  last() {
-    return this.todos[this.size() - 1];
-  }
-
-  itemAt(index) {
-    this._validateIndex(index);
-    return this.todos[index];
-  }
-
-  _validateIndex(index) {
-    if (!(index in this.todos)) {
-      throw new ReferenceError(`Invalid index: ${index}`);
-    }
-  }
-
-  markDoneAt(index) {
-    this.itemAt(index).markDone();
-  }
-
-  markUndoneAt(index) {
-    this.itemAt(index).markUndone();
-  }
-
-  isDone() {
-    return this.todos.every((todo) => todo.isDone());
-  }
-
-  shift() {
-    return this.todos.shift();
-  }
-
-  pop() {
-    return this.todos.pop();
-  }
-
-  removeAt(index) {
-    this._validateIndex(index);
-    return this.todos.splice(index, 1)[0];
-  }
-
-  toString() {
-    let title = `---- ${this.title} ----`;
-    let list = this.todos.map((todo) => todo.toString()).join("\n");
-    return `${title}\n${list}`;
-  }
-
-  forEach(callback) {
-    this.todos.forEach(callback);
-  }
-
-  filter(callback) {
-    let newList = new TodoList(this.title);
-    this.forEach((todo) => {
-      if (callback(todo)) {
-        newList.add(todo);
-      }
-    });
-
-    return newList;
-  }
-
-  findByTitle(title) {
-    return this.filter((todo) => todo.geTitle() === title).first();
-  }
-
-  allDone() {
-    return this.filter((todo) => todo.isDone());
-  }
-
-  allNotDone() {
-    return this.filter((todo) => !todo.isDone());
-  }
-
-  markDone(title) {
-    let toDoItem = this.findByTitle(title);
-    if (toDoItem !== undefined) {
-      toDoItem.markDone();
-    }
-  }
-
-  markAllDone() {
-    this.forEach((todo) => todo.markDone());
-  }
-
-  markAllUndone() {
-    this.forEach((todo) => todo.markUndone());
-  }
-
-  toArray() {
-    return this.todos.slice();
-  }
-}
+module.exports = Todo;
