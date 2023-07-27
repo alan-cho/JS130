e790b30c10da965ecdabf2c4e2018f32bd541b2e
Alan Cho <alancho397@gmail.com>
2023-07-26 21:42:58 -0500

    added solution five

--- Exercises/IIFEs.js
+++ Exercises/IIFEs.js
@@ -48,3 +48,87 @@ function delegate(object, method, ...additionalArgs) {
 */
 
 // 5)
+/*
+let Account = (function () {
+  let userEmail;
+  let userPassword;
+  let userFirstName;
+  let userLastName;
+
+  function isValidPassword(password) {
+    return userPassword === password;
+  }
+
+  function getRandomCharacter() {
+    let randomIndex = Math.floor(Math.random() * 62);
+    return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"[
+      randomIndex
+    ];
+  }
+
+  function anonymize() {
+    let result = "";
+
+    for (let index = 0; index < 16; index += 1) {
+      result += getRandomCharacter();
+    }
+
+    return result;
+  }
+
+  return {
+    init(email, password, firstName, lastName) {
+      userEmail = email;
+      userPassword = password;
+      userFirstName = firstName;
+      userLastName = lastName;
+      this.displayName = anonymize();
+      return this;
+    },
+
+    reanonymize(password) {
+      if (isValidPassword(password)) {
+        this.displayName = anonymize();
+        return true;
+      } else {
+        return "Invalid Password";
+      }
+    },
+
+    resetPassword(currentPassword, newPassword) {
+      if (isValidPassword(currentPassword)) {
+        userPassword = newPassword;
+        return true;
+      } else {
+        return "Invalid Password";
+      }
+    },
+
+    firstName(password) {
+      if (isValidPassword(password)) {
+        return userFirstName;
+      } else {
+        return "Invalid Password";
+      }
+    },
+
+    lastName(password) {
+      if (isValidPassword(password)) {
+        return userLastName;
+      } else {
+        return "Invalid Password";
+      }
+    },
+
+    email(password) {
+      if (isValidPassword(password)) {
+        return userEmail;
+      } else {
+        return "Invalid Password";
+      }
+    },
+  };
+})();
+*/
+
+// 6)
