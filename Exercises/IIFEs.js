6686f9ae98c74bcb349d71e4ec4eb8a2cbd244d5
Alan Cho <alancho397@gmail.com>
2023-07-26 21:19:44 -0500

    added solution two

--- Exercises/IIFEs.js
+++ Exercises/IIFEs.js
@@ -1,6 +1,20 @@
 // 1)
+/*
 function myBind(func, context) {
   return function () {
     return func.apply(context);
   };
 }
+*/
+
+// 2)
+/*
+function myBind(func, context, ...additionalArgs) {
+  return function (...arguments) {
+    const allArguments = additionalArgs.concat(arguments);
+    return func.apply(context, ...allArguments);
+  };
+}
+*/
+
+// 3)
